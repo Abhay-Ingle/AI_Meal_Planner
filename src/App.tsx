c6cb28e7fe13e6c477216f598092560b4e9e0441
rajrajputr68 <85095715+rajrajputr68@users.noreply.github.com>
2025-10-09 12:13:07 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
     - Goal: ${formData.goal}
     - Daily Calories: ${formData.calorieGoal} kcal
     - Meals per day: Breakfast, Lunch, Snack, Dinner
-    - Local Maharashtrian foods only.
+    - Make a proper personalised diet plan according to the height weight and age which will give the real results . Make the diet considering a he/she living in tier 3 and 4 city from maharashtra ,diet should be according to Maharashtrian meals and food choices and it should have only 4 meals per day, he/she is economically not good so give the plan which will be relevant , easy to understand and follow, should afford following the diet, no fancy meals.
 
     JSON structure example:
     {
@@ -296,3 +296,4 @@ function App() {
 }
 
 export default App;
+
